ef9d847284b1f0fd118f1570af146106d5ae9992
Damien Kingsley <actuallydamo@gmail.com>
2025-02-23 22:44:30 +1100

    build: switch from eslint and prettier to biome
    I want to try out Biome for linting and formatting.

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['standard-with-typescript', 'plugin:jest/recommended'],
-  parserOptions: {
-    project: './tsconfig.json'
-  },
-  ignorePatterns: ['/dist/'],
-  plugins: ['jest']
-}
